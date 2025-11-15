8fb14d711c98037c3674356374366897bd2e566d
hamor <hamedmoradi.mailsbox@gmail.com>
2025-11-15 22:23:52 +0330

    array getConcatenation exercise

---
+++ src/main/java/array/ConcatenationOfArray.java
@@ -0,0 +1,32 @@
+package array;
+
+import java.util.Arrays;
+
+public class ConcatenationOfArray {
+    public int[] getConcatenation(int[] nums) {
+        int[] con = new int[2 * nums.length];
+        int index = 0;
+        for (int i = 0; i < con.length; i++) {
+            if(i<nums.length){
+                con[i] = nums[i];
+            }else {
+                con[i] =nums[index++];
+            }
+        }
+        return con;
+    }
+//    public int[] getConcatenation(int[] nums) {
+//        int n = nums.length;
+//        int[] ans = new int[2*n];
+//        for (int i=0;i<2*n;i++) {
+//            ans[i] = nums[i%n];
+//        }
+//        return ans;
+//    }
+
+    public static void main(String[] args) {
+        ConcatenationOfArray c = new ConcatenationOfArray();
+        int[] nums = new int[]{1,2,3,4};
+        System.out.println(Arrays.toString(c.getConcatenation(nums)));
+    }
+}
