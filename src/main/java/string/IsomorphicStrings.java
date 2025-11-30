3c72ee36b26dc3b54275427f90a5ec4534a104ad
h.moradi <hamedmoradi.mailsbox@gmail.com>
2025-11-30 08:52:47 +0330

    String new exercise

---
+++ src/main/java/string/IsomorphicStrings.java
@@ -0,0 +1,37 @@
+package string;
+
+import java.util.HashMap;
+
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Character> mapST = new HashMap<>();
+        HashMap<Character, Character> mapTS = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+
+            if (mapST.containsKey(a)) {
+                if (mapST.get(a) != b) return false;
+            } else {
+                mapST.put(a, b);
+            }
+
+            if (mapTS.containsKey(b)) {
+                if (mapTS.get(b) != a) return false;
+            } else {
+                mapTS.put(b, a);
+            }
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        IsomorphicStrings isomorphicStrings = new IsomorphicStrings();
+        System.out.println(isomorphicStrings.isIsomorphic("badc", "baba"));
+    }
+}
